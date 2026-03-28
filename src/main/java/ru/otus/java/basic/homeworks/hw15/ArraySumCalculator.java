5476d499e52c17266f6087ec00c4e49b394c2df0
Stanislava <stanislava.kraft@gmail.com>
2026-03-28 19:32:53 +0300

    Домашнее задание "Исключения" правки согласно комментариям

--- src/main/java/ru/otus/java/basic/homeworks/hw15/ArraySumCalculator.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw15/ArraySumCalculator.java
@@ -1,29 +1,50 @@
 package ru.otus.java.basic.homeworks.hw15;
 
 public class ArraySumCalculator {
+    private static final int EXPECTED_SIZE = 4;
 
     public static int sumArrayElements(String[][] array)
             throws AppArraySizeException, AppArrayDataException {
 
-        if (array.length != 4) {
-            throw new AppArraySizeException("Массив должен быть размером 4x4");
+        if (array == null) {
+            throw new AppArraySizeException(
+                    "Массив не может быть null. Ожидаемый размер: " + EXPECTED_SIZE + "x" + EXPECTED_SIZE
+            );
         }
 
-        for (int i = 0; i < 4; i++) {
-            if (array[i].length != 4) {
-                throw new AppArraySizeException("Массив должен быть размером 4x4");
+        if (array.length != EXPECTED_SIZE) {
+            throw new AppArraySizeException(
+                    "Неверное количество строк. Ожидалось: " + EXPECTED_SIZE +
+                            ", получено: " + array.length
+            );
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                throw new AppArraySizeException(
+                        "Строка " + i + " равна null. Ожидаемый размер: " + EXPECTED_SIZE + "x" + EXPECTED_SIZE
+                );
+            }
+
+            if (array[i].length != EXPECTED_SIZE) {
+                throw new AppArraySizeException(
+                        "Неверное количество столбцов в строке " + i +
+                                ". Ожидалось: " + EXPECTED_SIZE +
+                                ", получено: " + array[i].length
+                );
             }
         }
 
         int sum = 0;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
                     throw new AppArrayDataException(
-                            "Невозможно преобразовать в число ячейку [" + i + "][" + j + "]: " + array[i][j]
+                            "Невозможно преобразовать в число ячейку [" + i + "][" + j +
+                                    "]: \"" + array[i][j] + "\""
                     );
                 }
             }
