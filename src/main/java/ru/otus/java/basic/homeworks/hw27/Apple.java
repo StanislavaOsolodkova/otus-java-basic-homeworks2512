518187b6c0f27d3b73c9f0a5ca296ffc1368e42e
Stanislava <stanislava.kraft@gmail.com>
2026-05-25 10:24:29 +0300

    Домашнее задание "Обобщения" выполнено согласно комментариям

--- src/main/java/ru/otus/java/basic/homeworks/hw27/Apple.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw27/Apple.java
@@ -2,6 +2,6 @@ package ru.otus.java.basic.homeworks.hw27;
 
 public class Apple extends Fruit {
     public Apple() {
-        super(0.2); // вес яблока, например, 0.2 кг
+        super(0.2);
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/hw27/Orange.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw27/Orange.java
@@ -2,6 +2,6 @@ package ru.otus.java.basic.homeworks.hw27;
 
 public class Orange extends Fruit {
     public Orange() {
-        super(0.15); // вес апельсина
+        super(0.15);
     }
 }
